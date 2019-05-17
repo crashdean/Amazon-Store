98875f38fa01a398be1b32fc1c4ac62d6340b82f
raerae1205 <crashdean@hotmail.com>
2019-05-16 19:56:25 -0700

    Adding Package.json and package-lock.json

--- amazonStoreSeed.sql
+++ amazonStoreSeed.sql
@@ -5,7 +5,7 @@ CREATE DATABASE amazon_storeDB;
 USE amazon_storeDB;
 
 CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
+  item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(45) NULL,
   department_name VARCHAR(45) NULL,
   price DECIMAL(10,2) NULL,
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
+  // Your port; if not 3307
   port: 3307,
 
   // Your username
