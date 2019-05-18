3e78e9f2dde637d5fde264d5629f9ede313554b5
raerae1205 <crashdean@hotmail.com>
2019-05-17 21:46:58 -0700

    Created item purchase and quantity update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -14,16 +15,58 @@ var connection = mysql.createConnection({
   database: "amazon_storeDB"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
-  });
-  
-  function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
-    });
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
   }
+
+  console.log('connected as id ' + connection.threadId);
+  start();
+});
+function start() {
+  // query the database for all items being auctioned
+  connection.query("SELECT * FROM products", function (err, results) {
+    //   console.log(results)
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: "itemId",
+          type: "list",
+          choices: function () {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].item_id);
+              // console.log(choiceArray);
+            }
+            return choiceArray;
+          },
+          message: "What product ID would you like to buy?",
+        },
+        {
+          name: "stockQuantity",
+          type: "input",
+          message: "How many units of the product would you like to buy?",
+        }
+
+
+      ])
+      .then(function (answer) {
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].item_id === answer.itemId) {
+            chosenItem = results[i];
+            console.log(chosenItem);
+
+            // purchaseItem();
+          }   
+        }
+        if (chosenItem.stock_quantity > answer.stockQuantity) {
+          console.log("You purchase was sucsessful!");
+        }
+      });
+  }
+  )
+}
